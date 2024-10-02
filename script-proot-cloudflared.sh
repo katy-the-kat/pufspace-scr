b418bd23d69da7bcfbe1d61bfac99be8a7c287a4
thestupidadmin <admin@is-a.space>
2024-10-02 14:56:07 +0300

    Update script-proot-cloudflared.sh

--- script-proot-cloudflared.sh
+++ script-proot-cloudflared.sh
@@ -31,4 +31,5 @@ echo Done installing cloudflared
 
 echo Use
 echo cloudflared tunnel --url http://localhost:8080
-echo to get access to your panel. Retry this command if it doesnt work or you restarted this container
+echo to get access to your panel. Retry this command if it doesnt work or you restarted this server
+echo Admin user is password adminadmin and email admin@is-a.space . You can change creds in panel
