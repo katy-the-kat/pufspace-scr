b62b2ba487e0cb01e6575d453162e9af37da73ac
thestupidadmin <admin@is-a.space>
2024-10-02 14:56:20 +0300

    Update script-proot.sh

--- script-proot.sh
+++ script-proot.sh
@@ -120,7 +120,7 @@ esac
 chmod +x /bin/portip
 echo Done installing ported / portip
 echo Now do port add 8080 to get port 8080 and access your panel
-
+echo Admin user is password adminadmin and email admin@is-a.space . You can change creds in panel
 
 
 
